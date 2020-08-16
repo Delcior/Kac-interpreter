957a71a482126efab8e45f546f3749a30d3fdb95
delcior <kacper.derlatka@outlook.com>
2020-08-16 14:17:21 +0200

    2nd branch test

--- src/com/kac/Lexer.java
+++ src/com/kac/Lexer.java
@@ -1,4 +1,19 @@
 package com.kac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexer {
+
+    private String source;
+
+    public Lexer(String source){
+        this.source=source;
+    }
+
+    public List<Token> scanTokens(){
+        System.out.println("scanning tokens");
+
+        return new ArrayList<Token>();
+    }
 }
