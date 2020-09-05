d45e3bfde8ec68407cd38e04daf9f09364da8778
delcior <kacper.derlatka@outlook.com>
2020-09-05 18:57:23 +0200

    primary() expression added

--- src/com/kac/Parser.java
+++ src/com/kac/Parser.java
@@ -19,8 +19,25 @@ public class Parser {
         return new Expr.Literal("token");
     }
 
-    //utility functions
+    private Expr primary(){
+        if(match(TokenType.NUMBER, TokenType.STRING))
+            return new Expr.Literal(previous().value);
 
+        if(match(TokenType.FALSE))
+            return new Expr.Literal(previous().value);
+        if(match(TokenType.TRUE))
+            return new Expr.Literal(previous().value);
+        if(match(TokenType.NULL))
+            return new Expr.Literal(null);
+
+        if(match(TokenType.LEFT_PAREN)){
+            Expr.Grouping groupingExpression = new Expr.Grouping(expression());
+            consume(TokenType.RIGHT_PAREN, "Expected ) after expression");
+            return groupingExpression;
+        }
+        return null;
+    }
+    //utility functions
     private Token peek(){
         return tokens.get(current);
     }
@@ -32,6 +49,7 @@ public class Parser {
     private boolean match(TokenType... types){
         for(TokenType type : types){
             if(check(type)) {
+                advance();
                 return true;
             }
         }
@@ -46,5 +64,16 @@ public class Parser {
 
     private Token advance(){
         return tokens.get(current++);
+    }
+
+    private Token previous(){
+        return tokens.get(current-1);
+    }
+
+    private Token consume(TokenType type, String message){
+        if(check(type))
+            return advance();
+
+        return null;
     }
 }
